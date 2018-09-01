f344ca79abda2e943683507e265c582755c0ddc2
Johnny Zavala <jzavalafgz@gmail.com>
2018-08-31 21:23:36 -0400

    added randomness button, smooth transitions

--- assets/pages.js
+++ assets/pages.js
@@ -5,23 +5,23 @@ let pageNumber = 0
 const pages = [
     {
         content:"a Queens-based graphic designer", 
-        backgroundColor: "",
-        circleColor: ""
+        background: "#edc7a9",
+        circle: "#3e78ed"
     },
     {
-        content:"Kanye West biggest fan", 
-        backgroundColor: "",
-        circleColor: ""
+        content:"Kanye West's biggest fan", 
+        background: "#a1fffe",
+        circle: "#e34a47"
     },
     {
         content:"looking for a job at the end of October", 
-        backgroundColor: "",
-        circleColor: ""
+        background: "#d3c7f3",
+        circle: "#f7fe00"
     },
     {
         content:`letting you <a href="pdf.pdf">download his PDF</a>`, 
-        backgroundColor: "",
-        circleColor: ""
+        background: "#faffb8",
+        circle: "#b472e6"
     },
 
 ]
@@ -29,7 +29,10 @@ const pages = [
 // pick the relevant tags
 const nextTag = document.querySelector("footer img.next")
 const prevTag = document.querySelector("footer img.prev")
+const randomTag =  document.querySelector("footer img.random")
 const outputTag = document.querySelector("h2")
+const circleTag = document.querySelector("section div.circle")
+const bodyTag = document.querySelector("body")
 
 //goes to the next page 
 const next = function ()
@@ -56,11 +59,22 @@ const prev = function ()
 	updateSection()
 }
 
+//goes to random page
+const random = function () {
+    pageNumber = chance.integer({ min: 0, max: pages.length - 1 })
+
+    updateSection()
+
+}
+
 //Updates content and style according to the page we are on
 const updateSection = function ()
 {
         outputTag.innerHTML = pages[pageNumber].content
+        bodyTag.style.backgroundColor = pages[pageNumber].background
+        circleTag.style.backgroundColor = pages[pageNumber].circle
 }
 // on click of tag, run corresponding function
 nextTag.addEventListener("click", next)
 prevTag.addEventListener("click", prev)
+randomTag.addEventListener("click", random)
